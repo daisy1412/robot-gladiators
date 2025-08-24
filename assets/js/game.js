b56faa73f44d154de7953e3804dc239193393acd
Daisy Nguyen <daisn1412@gmail.com>
2025-08-24 15:22:15 -0500

    Add fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,26 @@
-window.alert("This is an alert. JavaScript is running");
+var playerName = window.alert("What's your robot's name?"); 
+var playerHealth = 100; 
+var playerAttack = 60; 
+
+console.log(playerName, playerHealth, playerAttack); 
+
+var enemyName = "Roborto"; 
+var enemyHealth = 50; 
+var enemyAttack = 12; 
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiator"); 
+    playerHealth = playerHealth - enemyAttack; 
+    console.log(playerName + " now has " + playerHealth + " health remaining"); 
+    enemyHealth = enemyHealth - playerAttack; 
+    console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining "); 
+
+    //check enemy health
+    if(enemyHealth <= 0){
+        window.alert(enemyName + " has died! "); 
+    }else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left"); 
+    }
+}
+fight(); 
+
