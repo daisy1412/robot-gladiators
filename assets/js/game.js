ed8f62abbb1524d6b73962f811113bb3648b436c
Daisy Nguyen <daisn1412@gmail.com>
2025-08-31 23:24:55 -0500

    starat adn end game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,11 @@
 var playerName = window.prompt("What's your robot's name?"); 
-var playerHealth = 100; 
+var playerHealth = 60; 
 var playerAttack = 10; 
 var playerMoney = 10; 
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 var enemyHealth = 50; 
-var enemyAttack = 12;
+var enemyAttack = 20;
 
 var fight = function(enemyName){
  
@@ -17,6 +17,7 @@ var fight = function(enemyName){
 
             if(enemyHealth <= 0){
                 window.alert(enemyName + " has died! "); 
+                window.alert(playerName + " still has " + playerHealth + " health remaining. ");
                 break; 
             }else{
                 window.alert(enemyName + " still has " + enemyHealth + " health left"); 
@@ -28,100 +29,58 @@ var fight = function(enemyName){
             //check player's Health
             if(playerHealth <= 0){
                 window.alert(playerName + " has died!"); 
+                window.alert(playerName + " still has " + playerHealth + " health remaining. ");
                 break;
             }else{
                 window.alert(playerName + " still has " + playerHealth + " health remaining. ");
             }
-        }if(promptFight == "skip" || promptFight == "SKIP"){
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
-        if (confirmSkip){
-            playerMoney = playerMoney - 10; 
-            window.alert(playerName + " has choosen to skip the fight. Goodbye!");
-            console.log(playerMoney, " player money");
-            break; 
-        }else{
-            fight();
-        }   
+        }else if(promptFight == "skip" || promptFight == "SKIP"){
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
+            if (confirmSkip){
+                playerMoney = playerMoney - 10; 
+                window.alert(playerName + " has choosen to skip the fight. Goodbye!");
+                console.log(playerMoney, " player money");
+                break; 
+            }else{
+                fight();
+            }   
         }else{
             window.alert("Invalid input. Choose again"); 
         }
     }
-    // var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."); 
-    // if(promptFight == "fight" || promptFight == "FIGHT"){
-        
-    //     while(enemyHealth > 0 && playerHealth > 0){
-    //         enemyHealth = enemyHealth - playerAttack; 
-    //         console.log(playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining ");
-
-    //         //update playerHealth
-    //         playerHealth = playerHealth - enemyAttack; 
-    //         console.log(playerName + " now has " + playerHealth + " health remaining");
-    //     }
-        
-    //     if(enemyHealth <= 0 && playerHealth > 0){
-    //         window.alert(enemyName + " has died! "); 
-    //         console.log(playerName + " still has " + playerHealth + " health remaining. ")
-    //     }else if(enemyHealth > 0 && playerHealth <= 0){
-    //         window.alert(playerName + " has died! "); 
-    //     }
-        
-    // }else if(promptFight == "skip" || promptFight == "SKIP"){
-    //     var confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
-    //     if (confirmSkip){
-    //         playerMoney = playerMoney - 2; 
-    //         window.alert(playerName + " has choosen to skip the fight. Goodbye!");
-    //         console.log(playerName + " now has " + playerMoney + " money remaining");
-    //     }else{
-    //         fight();
-    //     }
-            
-    // }else{
-    //     window.alert("Invalid input. Choose again"); 
-    // }     
-
-        //check enemy health
-    //     if(enemyHealth <= 0){
-    //         window.alert(enemyName + " has died! "); 
-    //     }else{
-    //         window.alert(enemyName + " still has " + enemyHealth + " health left"); 
-    //     }
-
-    //     //update playerHealth
-    //     playerHealth = playerHealth - enemyAttack; 
-    //     console.log(playerName + " now has " + playerHealth + " health remaining");
-        
-    //     //check player's Health
-    //     if(playerHealth <= 0){
-    //         window.alert(playerHealth + " has died!"); 
-    //     }else{
-    //         window.alert(playerName + " still has " + playerHealth + " health remaining. ");
-    //     }
-    // }else if(promptFight == "skip" || promptFight == "SKIP"){
-    //     var confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
-    //     if (confirmSkip){
-    //         playerMoney = playerMoney - 2; 
-    //         window.alert(playerName + " has choosen to skip the fight. Goodbye!");
-    //         console.log(playerName + " now has " + playerMoney + " money remaining");
-    //     }else{
-    //         fight();
-    //     }
-            
-    // }else{
-    //     window.alert("Invalid input. Choose again"); 
-    // }  
-     
 }
-
-for(var i=0; i < enemyNames.length; i++){ 
-    enemyHealth = 50; 
-    if(playerHealth > 0){
-        window.alert("Welcome to Robot Gladiator! Round " + (i+1)); 
-        fight(enemyNames[i]);
-    }else{
-        window.alert("You have lost. Game over!"); 
-        break; 
+var startGame = function(){
+    playerAttack = 10;
+    playerHealth = 100; 
+    playerMoney = 10; 
+    for(var i=0; i < enemyNames.length; i++){ 
+        enemyHealth = 50; 
+        if(playerHealth > 0){
+            window.alert("Welcome to Robot Gladiator! Round " + (i+1)); 
+            var pickEnemyName = enemyNames[i]; 
+            fight(pickEnemyName);
+        }else{
+            window.alert("You have lost. Game over!"); 
+            break; 
+        }
     }
-    
+    //player now has no health or no enemies to fight. 
+    endGame(); 
 }
+var endGame = function(){
+    if(playerHealth > 0){
+        window.alert("YOU WIN. You have earned " + playerMoney + " money. "); 
+        var playAgain = window.confirm("Would you like to play again? "); 
+        if(playAgain){
+            startGame(); 
+        }else{
+            window.alert("Thank you for playing. See you again soon!"); 
+        }
+    }else{
+        window.alert("Sorry, you have lost."); 
+    } 
+}
+startGame(); 
+
  
 
